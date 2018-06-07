f3e7b3bfe37de15c82dc57e923cfcee527c57759
Cosciad <cosciadani@gmail.com>
2018-06-07 17:14:41 +0200

    solved problem with bullets and walls

--- game.sample/onscreen/Entity.java
+++ game.sample/onscreen/Entity.java
@@ -68,7 +68,9 @@ public class Entity {
 	}
 
 	public void hit(Model model) {
-		new Hit().execute(model, this);
+		Hit h = new Hit();
+		if (h.canihit(model, this))
+			new Hit().execute(model, this);
 	}
 
 	public void explode(Model model) {
--- game.sample/onscreen/Hit.java
+++ game.sample/onscreen/Hit.java
@@ -7,6 +7,26 @@ public class Hit extends Action {
 	public Hit() {
 	}
 
+	public boolean canihit(Model model, Entity e) {
+		Point p = new Point(e.p);
+
+		switch (e.dir) {
+		case 'D':
+			p.i++;
+			break;
+		case 'L':
+			p.j--;
+			break;
+		case 'R':
+			p.j++;
+			break;
+		default:
+			p.i--;
+			break;
+		}
+		return model.m.map[p.i][p.j].type == 'F';
+	}
+
 	public void execute(Model model, Entity e) {
 		State s = new State("1");
 		Transition[] transitionsb = new Transition[2];
@@ -18,7 +38,6 @@ public class Hit extends Action {
 		transitionsb[1] = new Transition(s, s, eAction, cond1);
 		Automate a = new Automate(model, s, transitionsb);
 		Bullet b = new Bullet(model.m_bullet, 1L, e, a, s);
-		/* if (model.m.map[b.p.i][b.p.j].type == 'F') */
 		model.add(b);
 	}
 }
