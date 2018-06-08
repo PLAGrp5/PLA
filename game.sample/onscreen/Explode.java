89c8086bf9944de9e21161a756b8e94098c5336e
Cosciad <cosciadani@gmail.com>
2018-06-08 10:35:37 +0200

    Perte de vie si touché par une balle

--- game.sample/onscreen/Explode.java
+++ game.sample/onscreen/Explode.java
@@ -11,6 +11,12 @@ public class Explode extends Action {
     }
 
     public void execute(Model model, Entity e) {
+        Point pe = new Point(e.p);
+        Point p = pe.nextPoint(e.dir);
+        Entity e1 = model.m.map[p.i][p.j];
+        e1.updatevie(-1);
         model.del(e);
+        if (e1.type != 'W' && e1.vie < 1)
+            model.del(e1);
     }
 }
