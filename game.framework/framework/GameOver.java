05bfd184ba00022a54f70ab445469a96ff7f59ff
MrJown <naes.lagrange@gmail.com>
2018-06-08 17:11:04 +0200

    Commencement de la Frame GameOver

--- game.framework/framework/GameOver.java
+++ game.framework/framework/GameOver.java
@@ -2,12 +2,16 @@ package framework;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,7 +22,6 @@ import framework.GameUI.STATE;
 public class GameOver {
 
 	JFrame overFrame;
-	JLabel headerLabel;
 	JPanel controlPanel;
 	GameUI g_ui;
 
@@ -30,9 +33,10 @@ public class GameOver {
 	public void prepareGUI() {
 		overFrame = new JFrame("Game Over");
 		overFrame.setSize(1024, 1024);
-		overFrame.setLayout(new GridLayout(3, 1));
-
-		headerLabel = new JLabel("", JLabel.CENTER);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		overFrame.setLocation(dim.width / 2 - overFrame.getSize().width / 2,
+				dim.height / 2 - overFrame.getSize().height / 2);
+		overFrame.setIconImage(new ImageIcon("game.sample/sprites/image.png").getImage());
 
 		overFrame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent windowEvent) {
@@ -40,22 +44,47 @@ public class GameOver {
 			}
 		});
 		
-		controlPanel = new JPanel();
-		controlPanel.setLayout(new FlowLayout());
+		// Gestion image de fond
+				ImageIcon imageicon = new ImageIcon("game.sample/sprites/landscape.jpg");
+				// L'image de fond est issue du site https://pixabay.com qui est une banque
+				// d'images libre de droits
+				Image image = imageicon.getImage();
 
-		overFrame.add(headerLabel);
-		overFrame.add(controlPanel);
+				controlPanel = new JPanel() {
+					@Override
+					protected void paintComponent(Graphics g) {
+						super.paintComponent(g);
+						g.drawImage(image, 0, 0, null);
+					}
+				};
+		controlPanel.setLayout(new GridLayout(4,1));
+
+		overFrame.setContentPane(controlPanel);
 		overFrame.setVisible(true);
 	}
 	
 	public void showEvent() {
-		headerLabel.setText("Game Over");
-		JButton ExitButton = new JButton("EXIT");
-		ExitButton.setActionCommand("EXIT");
+		MyButton MenuButton = new MyButton("MENU", "game.sample/sprites/bleu.jpg", "game.sample/sprites/rouge.png");
+		MenuButton.setActionCommand("MENU");
 		
-		ExitButton.addActionListener(new ButtonClickListener());
+		JLabel label = new JLabel(); // Création d'un JLabel contenant le logo du jeu
+		label.setIcon(new ImageIcon("game.sample/sprites/GameOver.png")); // Image du logo
+		// Logo créé sur le site web https://cooltext.com
+		// Il est écrit que l'on peut utiliser les images créées sur leur site.
+		label.setHorizontalAlignment(javax.swing.SwingConstants.CENTER); // Centrer le logo
 		
-		controlPanel.add(ExitButton);
+		MenuButton.addActionListener(new ButtonClickListener());
+		MenuButton.setPreferredSize(new Dimension(250, 100));
+		
+		
+		JPanel panelBouton = new JPanel();
+		panelBouton.setOpaque(false);
+		panelBouton.add(MenuButton);
+		
+		controlPanel.add(label);
+		controlPanel.add(new JLabel());
+		controlPanel.add(new JLabel());
+		controlPanel.add(panelBouton);
 		
 		overFrame.setVisible(true);
 	}
@@ -64,7 +93,7 @@ public class GameOver {
 		public void actionPerformed(ActionEvent e) {
 			String command = e.getActionCommand();
 
-			if (command.equals("EXIT")) {
+			if (command.equals("MENU")) {
 				g_ui.setState(STATE.Menu);
 				Dimension d = new Dimension(1024, 1024);
 				overFrame.dispose();
