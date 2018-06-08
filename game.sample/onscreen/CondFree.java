c34bfb0190f8c9d81114efec3f2de3928abc77d6
chabrema <manon.chabre@gmail.com>
2018-06-08 13:25:33 +0200

    resolution probleme de move des automate

--- game.sample/onscreen/CondFree.java
+++ game.sample/onscreen/CondFree.java
@@ -1,25 +1,44 @@
 package onscreen;
 
-public class CondFree extends Condition{
-	
+public class CondFree extends Condition {
+
 	public CondFree(Map m) {
 		this.m = m;
 	}
 
-	public boolean eval(Entity ent) {
-		switch (ent.dir) {
-		case 'U': 
-			return (m.isfree(ent.p.i-1, ent.p.j));
-		case 'D':
-			return (m.isfree(ent.p.i+1, ent.p.j));
-		case 'L':
-			return (m.isfree(ent.p.i, ent.p.j-1));
-		case 'R':
-			return (m.isfree(ent.p.i, ent.p.j+1));
-		default:
-			return false;
+	// retourne vrai si le deplacement est possible (la case devant est free ou un
+	// bonus)
+	boolean canimove(Map m, int i, int j) {
+		return m.isfree(i, j) || m.isbonus(i, j) || m.ismine(i, j) || m.isbullet(i, j);
+	}
 
-		
+	public boolean eval(Entity ent) {
+		if (ent instanceof Sbire) {
+			switch (ent.dir) {
+				case 'U':
+					return (canimove(m, ent.p.i - 1, ent.p.j));
+				case 'D':
+					return (canimove(m, ent.p.i + 1, ent.p.j));
+				case 'L':
+					return (canimove(m, ent.p.i, ent.p.j - 1));
+				case 'R':
+					return (canimove(m, ent.p.i, ent.p.j + 1));
+				default:
+					return false;
+			}
+		} else {
+			switch (ent.dir) {
+				case 'U':
+					return (m.isfree(ent.p.i - 1, ent.p.j));
+				case 'D':
+					return (m.isfree(ent.p.i + 1, ent.p.j));
+				case 'L':
+					return (m.isfree(ent.p.i, ent.p.j - 1));
+				case 'R':
+					return (m.isfree(ent.p.i, ent.p.j + 1));
+				default:
+					return false;
+			}
 		}
-	}	
+	}
 }
