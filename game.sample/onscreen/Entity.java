c862292f95727e9a708da2581a556e2ae07a7a33
Cosciad <cosciadani@gmail.com>
2018-06-12 10:39:32 +0200

    bug fixed

--- game.sample/onscreen/Entity.java
+++ game.sample/onscreen/Entity.java
@@ -32,7 +32,7 @@ public class Entity {
 	int vie_max;
 
 	public boolean alive = true;
-	
+
 	public BonusEtMalus[] inventaire = new BonusEtMalus[3];
 	public int jauge_couleur;
 	public int lastj, lasti;
@@ -50,17 +50,20 @@ public class Entity {
 		this.type = type;
 		p.i = 1;
 		p.j = 1;
+		alive = false;
 	}
 
 	public Entity(char type, int i, int j, char dir) {
 		this.type = type;
 		this.p = new Point(i, j);
 		this.dir = dir;
+		alive = false;
 	}
 
 	public Entity(char type, int i, int j) {
 		this.type = type;
 		this.p = new Point(i, j);
+		alive = false;
 	}
 
 	public Entity(Model model, BufferedImage sprite, int x, int y, char dir, float scale) {
@@ -70,22 +73,23 @@ public class Entity {
 		p.j = x;
 		this.dir = dir;
 		m_scale = scale;
+		alive = false;
 	}
 
 	public void opposite() {
 		switch (this.dir) {
-			case 'S':
-				this.dir = 'N';
-				break;
-			case 'W':
-				this.dir = 'E';
-				break;
-			case 'E':
-				this.dir = 'W';
-				break;
-			default:
-				this.dir = 'S';
-				break;
+		case 'S':
+			this.dir = 'N';
+			break;
+		case 'W':
+			this.dir = 'E';
+			break;
+		case 'E':
+			this.dir = 'W';
+			break;
+		default:
+			this.dir = 'S';
+			break;
 		}
 	}
 
@@ -123,8 +127,8 @@ public class Entity {
 		}
 	}
 
-	public void explode() {
-		new Explode().execute(this);
+	public void kamikaze() {
+		new Kamikaze().execute(this);
 	}
 
 	public void turn(char dir) {
@@ -157,10 +161,11 @@ public class Entity {
 	}
 
 	public void updatevie(Model model, int vie) {
+
 		this.vie += vie;
 		if (this.vie < 1) {
 			this.vie = 0;
-			if (type != 'W' && type != 'P')
+			if (alive)
 				model.del(this);
 		}
 	}
